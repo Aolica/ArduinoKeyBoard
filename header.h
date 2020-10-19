bf782a7733deaf24d56b59d164eb9bea199e29f2
Aolica <or64000@gmail.com>
2020-10-19 16:28:31 +0900

    [add] ノブの制御を追加
    [add] ライブラリの変更

--- header.h
+++ header.h
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-struct stat {     //押されたときの情報を保持するための構造体。
+struct keystat {     //押されたときの情報を保持するための構造体。
   int ispushed; //PUSH_TRUE:pushed
   //byte semitone //value:0-11 (0:key=C)
   byte octave; //value:0-10
@@ -8,7 +8,7 @@ struct stat {     //押されたときの情報を保持するための構造体
 };
 
 bool IsSharp(byte key) {
-  if ((key  > 6 && key % 2 != 0 ) || ( key  < 5 && key % 2 == 0)) {
+  if ((key  >= 6 && key % 2 == 0) || ( key  < 5 && key % 2 != 0)) {
     return true;
   }
   return false;
@@ -44,13 +44,13 @@ char KeyNumToAlph(byte num) {
   }
 }
 
-void SendKeyData(int type,struct stat data) {
+void SendKeyData(int type,struct keystat data) {
   char datas[10];
   /*
   if(data.key + data.semitone > 12){
     data.octave + 1;
   }*/
-  sprintf(datas,"%d%c%u%u%u",1,KeyNumToAlph(data.key),IsSharp(data.key),data.octave,type);
+  sprintf(datas,"%d%c%u%u%u%u",1,KeyNumToAlph(data.key),IsSharp(data.key),data.octave,type,data.key);
   Serial.print(datas);
   
   /*
@@ -64,4 +64,10 @@ void SendKeyData(int type,struct stat data) {
   }
   Serial.write(data.ispushed);
   */
+}
+
+void SendKnobData(int rdirection,int id){
+  char datas[5];
+  sprintf(datas,"%d%d%d",2,id,rdirection);
+  Serial.print(datas);
 }
--- main.ino
+++ main.ino
@@ -4,11 +4,17 @@
 #define KEYS 24
 #define PUSH_TRUE LOW
 #define PUSH_FALSE HIGH
+#define KNOBS 3
+const int rotateref[] = {2, 0, 1, 2, 1, 2, 2, 0, 0, 2, 2, 1, 2, 1, 0, 2}; //check rotate direction (0:right,1:left,2:stop)
+//////////////////////////////////////////////////////////////////////////////////////////
 Adafruit_MCP23017 mcp1, mcp2;
 byte octave = 3; //右端のCのオクターブ
 byte semitone = 0; //はじめのkey(key=C)
 int buf;
 struct keystat keykeystatus[KEYS];
+//////////////////////////////////////////////////////////////////////////////////////////
+int rbuf = 0;
+int knobstate[KNOBS] = {};
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -18,12 +24,15 @@ void setup() {
   mcp2.begin(0x01);
   for (int i = 0; i <= 15; i++) {
     mcp1.pinMode(i, INPUT);
-    mcp1.pullUp(i,HIGH);
+    mcp1.pullUp(i, HIGH);
     mcp2.pinMode(i, INPUT);
-    mcp2.pullUp(i,HIGH);
+    mcp2.pullUp(i, HIGH);
   }
   for (int i = 0; i < KEYS; i++) {  //キーのステータスの初期化
     keykeystatus[i].ispushed = PUSH_FALSE;
+  }
+  for (int i = 2; i < (KNOBS * 2 + 2); i++) {           //tx,rxはシリアル通信の都合で使えない
+    pinMode(i, INPUT_PULLUP); //for rotaly switch A
   }
 }
 
@@ -49,16 +58,24 @@ void loop() {
         //keykeystatus[i].semitone = semitone;
         keykeystatus[i].octave   = octave + (byte)(int)((i + semitone) / 12.0);      //オクターブの設定
         keykeystatus[i].key      = (i + semitone) % 12;
-        SendKeyData(1,keykeystatus[i]);
+        SendKeyData(1, keykeystatus[i]);
       } else {                                 //離されたときの処理
         keykeystatus[i].ispushed = PUSH_FALSE;
-        SendKeyData(0,keykeystatus[i]);
+        SendKeyData(0, keykeystatus[i]);
       }
     }
   }
   //////////////////////////////////////////////////////////////////////////////////////////
   //つまみの読み込み処理
   //////////////////////////////////////////////////////////////////////////////////////////
-
+  for (int i = 0; i < KNOBS ; i++) {
+    knobstate[i] = (knobstate[i] << 2 | ((PIND >> 2 * (i + 1)) & 0x03)) & 0x0f;
+    rbuf = rotateref[knobstate[i]];
+    //  Serial.println(PIND, BIN);
+    //  Serial.println("　");
+    if (rbuf != 2) {
+      SendKnobData(rbuf,i);
+    }
+  }
   // delay(1);        // delay in between reads for stability
 }
